b74ebe8f70eca7c03ad361e96bfe807615401f70
Przyborek <martyna.przyborek@cgi.com>
2019-03-01 15:25:49 +0100

    a

--- database/schema/create_schema.sql
+++ database/schema/create_schema.sql
@@ -13,6 +13,23 @@ CREATE SCHEMA AUTHORIZATION shifty
      CONSTRAINT fk_country_id FOREIGN KEY(country_id) REFERENCES countries(country_id),
      CONSTRAINT unique_holiday UNIQUE (country_id, holiday_date)
     )
+  CREATE TABLE members
+    (member_id NUMBER(6,0) PRIMARY KEY,
+    member_name VARCHAR2(50) NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL
+    )
+    CREATE TABLE shift_definition
+    (shift_name VARCHAR2(15) PRIMARY KEY,
+    start_time VARCHAR2(6) NOT NULL,
+    end_time VARCHAR2(6) NOT NULL
+    )
+    CREATE TABLE shifts
+    (shift_name VARCHAR2(15) NOT NULL,
+    member_id NUMBER(6,0),
+    CONSTRAINT fk_member_id FOREIGN KEY(member_id) REFERENCES members(member_id),
+    CONSTRAINT fk_shift_name FOREIGN KEY(shift_name) REFERENCES shift_definition(shift_name)
+    )
   GRANT SELECT ON countries TO shifty_app
   GRANT SELECT ON holidays  TO shifty_app
   GRANT INSERT ON holidays  TO shifty_app
--- database/testdata/populate.sql
+++ database/testdata/populate.sql
@@ -9,4 +9,15 @@ INSERT INTO holidays VALUES('FI', TO_DATE('24/12/2019', 'DD/MM/YYYY'), 'Christma
 INSERT INTO holidays VALUES('FI', TO_DATE('25/12/2019', 'DD/MM/YYYY'), 'Christmas Day 1');
 INSERT INTO holidays VALUES('FI', TO_DATE('26/12/2019', 'DD/MM/YYYY'), 'Christmas Day 2');
 
+INSERT INTO countries VALUES('PL', 'Poland');
+INSERT INTO countries VALUES('FI', 'Finaland');
+
+INSERT INTO members VALUES('1', 'Martyna Przyborek', TO_DATE('01/01/2019', 'DD/MM/YYYY;'), TO_DATE('31/12/2019', 'DD/MM/YYYY'));
+INSERT INTO members VALUES('2', 'Olga Bryś', TO_DATE('01/01/2019', 'DD/MM/YYYY;'), TO_DATE('31/12/2019', 'DD/MM/YYYY'));
+INSERT INTO members VALUES('3', 'Tomasz Kaim', TO_DATE('01/01/2019', 'DD/MM/YYYY;'), TO_DATE('31/12/2019', 'DD/MM/YYYY'));
+INSERT INTO members VALUES('4', 'Samuli Latvala', TO_DATE('01/01/2019', 'DD/MM/YYYY;'), TO_DATE('31/12/2019', 'DD/MM/YYYY'));
+
+INSERT INTO shift_definition VALUES('Early Shift', '06:30', '14:30');
+INSERT INTO shift_definition VALUES('Late Shift', '09:00', '17:00');
+
 COMMIT;
