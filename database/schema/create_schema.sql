4c221897e9631c7a3e4b3f48000b3b3c8ebffe9b
Przyborek <martyna.przyborek@cgi.com>
2019-03-13 13:01:29 +0100

    new table added

--- database/schema/create_schema.sql
+++ database/schema/create_schema.sql
@@ -20,13 +20,14 @@ CREATE SCHEMA AUTHORIZATION shifty
     end_date date NOT NULL
     )
     CREATE TABLE shift_definition
-    (shift_name VARCHAR2(15) PRIMARY KEY,
+    (id NUMBER PRIMARY KEY
+    shift_name VARCHAR2(15) NOT NULL,
     start_time VARCHAR2(6) NOT NULL,
     end_time VARCHAR2(6) NOT NULL
     )
     CREATE TABLE shifts
     (shift_name VARCHAR2(15) NOT NULL,
-    member_id NUMBER(6,0),
+    member_id NUMBER(6,0) NOT NULL,
     CONSTRAINT fk_member_id FOREIGN KEY(member_id) REFERENCES members(member_id),
     CONSTRAINT fk_shift_name FOREIGN KEY(shift_name) REFERENCES shift_definition(shift_name)
     )
@@ -34,4 +35,16 @@ CREATE SCHEMA AUTHORIZATION shifty
   GRANT SELECT ON holidays  TO shifty_app
   GRANT INSERT ON holidays  TO shifty_app
   GRANT UPDATE ON holidays  TO shifty_app
-  GRANT DELETE ON holidays  TO shifty_app;
+  GRANT DELETE ON holidays  TO shifty_app
+  GRANT SELECT ON members   TO shifty_app
+  GRANT INSERT ON members   TO shifty_app
+  GRANT UPDATE ON members   TO shifty_app
+  GRANT DELETE ON members   TO shifty_app
+  GRANT SELECT ON shifts    TO shifty_app
+  GRANT INSERT ON shifts    TO shifty_app
+  GRANT UPDATE ON shifts    TO shifty_app
+  GRANT DELETE ON shifts    TO shifty_app
+  GRANT SELECT ON shift_definition  TO shifty_app
+  GRANT INSERT ON shift_definition  TO shifty_app
+  GRANT UPDATE ON shift_definition  TO shifty_app
+  GRANT DELETE ON shift_definition  TO shifty_app;
